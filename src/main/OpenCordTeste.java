90fae24a97359b7f3dec5e9be66ebae9fd76cda5
ruben <ruben@rubenfranca>
2020-12-07 09:46:35 -0300

    Inicial

---
+++ src/main/OpenCordTeste.java
@@ -0,0 +1,71 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+
+public class OpenCordTeste {
+//	private static final Logger log = Logger.getLogger(LocalShell.class.getName());
+//	private static final Logger log = Logger.getLogger(Local.)
+	
+	public static void main(String[] args) {
+//		final LocalShell shell = new LocalShell();
+//        shell.executeCommand("ls ~");
+		try {
+			executeCommand("cd ~/Documentos && ./teste.sh ");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void executeCommand(final String command) throws IOException {
+
+        final ArrayList<String> commands = new ArrayList<String>();
+        commands.add("/bin/bash");
+        commands.add("-c");
+        commands.add(command);
+
+        BufferedReader br = null;
+
+        try {
+            final ProcessBuilder p = new ProcessBuilder(commands);
+            final Process process = p.start();
+            final InputStream is = process.getInputStream();
+            final InputStreamReader isr = new InputStreamReader(is);
+            br = new BufferedReader(isr);
+
+            String line;
+            while((line = br.readLine()) != null) {
+                System.out.println("Retorno do comando = [" + line + "]");
+            }
+        } catch (IOException ioe) {
+//            log.severe("Erro ao executar comando shell" + ioe.getMessage());
+            throw ioe;
+        } finally {
+            secureClose(br);
+        }
+    }
+ 
+    private static void secureClose(final Closeable resource) {
+        try {
+            if (resource != null) {
+                resource.close();
+            }
+        } catch (IOException ex) {
+//            log.severe("Erro = " + ex.getMessage());
+        }
+    }
+
+//    public static void main (String[] args) throws IOException {
+//        final LocalShell shell = new LocalShell();
+//        shell.executeCommand("ls ~");
+//    }
+	
+}
